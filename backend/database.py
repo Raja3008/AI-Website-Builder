b07f0f6def202a13a2d5ea73f7734f3d073cb76c
Raja3008 <157313007+Raja3008@users.noreply.github.com>
2026-03-27 12:03:25 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://mrajashekar482_db_user:VOMvHR31aFPDyxvu@cluster0.ceywd3f.mongodb.net/?appName=Cluster0"
+MONGO_URI = ""
 
 client = MongoClient(MONGO_URI)
 
